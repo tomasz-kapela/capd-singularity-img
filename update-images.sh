840d2ea34fc2bc67621959bb058f43fd9ee52447
Tomasz Kapela <92725383+tomasz-kapela@users.noreply.github.com>
2023-02-17 19:21:15 +0100

    Create update-images.sh

---
+++ update-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo singularity build capd-dev.sif capd-dev.def
+singularity sign capd-dev.sif
+singularity push capd-dev.sif library://capdnet/capd/capd-dev:latest
+
+sudo singularity build capd.sif capd.def
+singularity sign capd.sif
+singularity push capd.sif library://capdnet/capd/capd:latest
