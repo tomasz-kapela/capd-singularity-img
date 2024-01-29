b6f421fe9a4c6e79322bd7234329dd296c209dea
Tomasz Kapela <tomasz.kapela@gmail.com>
2024-01-29 19:56:48 +0100

    New capd version

--- update-images.sh
+++ update-images.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo singularity build capd-dev.sif capd-dev.def
+sudo singularity build -F capd-dev.sif capd-dev.def
 singularity sign capd-dev.sif
 singularity push capd-dev.sif library://capdnet/capd/capd-dev:latest
 
-sudo singularity build capd.sif capd.def
+sudo singularity build -F capd.sif capd.def
 singularity sign capd.sif
 singularity push capd.sif library://capdnet/capd/capd:latest
